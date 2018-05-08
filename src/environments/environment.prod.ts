705bf62af9a572bf6866399df403077115f6747c
brian.monroy <brayant.mejia@intelmas.com>
2018-05-08 11:21:20 -0500

    adding components, class and service basic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEg94DGJu4Xf_fGua4fb52ysYK0z7BhA8",
+    authDomain: "angular5-crud-b366c.firebaseapp.com",
+    databaseURL: "https://angular5-crud-b366c.firebaseio.com",
+    projectId: "angular5-crud-b366c",
+    storageBucket: "angular5-crud-b366c.appspot.com",
+    messagingSenderId: "1032386966231"
+  }
 };
